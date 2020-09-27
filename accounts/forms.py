d73eafca47b679ef4ce73b0794806faf2ccdcfaf
Basia <barbara.budnik@wp.pl>
2020-09-27 10:34:03 +0200

    next comit

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,9 +2,11 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout,Submit
 
 from django.contrib.auth.forms import (AuthenticationForm,PasswordChangeForm,UserCreationForm)
-from django.forms import Form
+from django.forms import Form, IntegerField
 
 from django.db.transaction import  atomic
+
+from accounts.models import Profile
 
 
 class SubmittableForm(Form):
@@ -19,5 +21,15 @@ class SubmittablePasswordChangeForm(SubmittableForm,PasswordChangeForm):
     pass
 
 class SignUpForm (SubmittableForm,UserCreationForm):
+    shoe_size=IntegerField(
+        label='size',
+    )
     class Meta(UserCreationForm.Meta):
         fields = ['username','first_name']
+
+    def save(self,commit=True,*args,**kwargs):
+        user=super().save(commit)
+        shoe_size=self.cleaned_data['shoe_size']
+        profile=Profile(shoe_size=shoe_size, user=user)
+        profile.save()
+        return user
